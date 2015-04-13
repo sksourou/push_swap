03b0e5ff142434b2f00d53559a007ee2522b16e5
Sofiane KSOUROU <sksourou@e1r9p19.42.fr>
2015-04-13 23:53:28 +0200

    propre

---
+++ src/check_error.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sksourou <sksourou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/04/07 20:41:43 by sksourou          #+#    #+#             */
+/*   Updated: 2015/04/07 20:41:45 by sksourou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	error(int ac, int *tab, char **av)
+{
+	int			e;
+	int			i;
+	long int  tmp;
+
+	e = 0;
+	while (e < ac - 1)
+	{
+		if (((tmp = ft_atoi(av[e + 1]))) > 3000|| tmp < -3000)
+			ft_putstr("int Error"), exit(1);
+		else
+			tab[e] = ft_atoi(av[e + 1]);
+		e++;
+	}
+	e = 0;
+	while (e < ac)
+	{
+		i = e + 1;
+		while (i < ac)
+		{
+			if (tab[e] == tab[i])
+				ft_putendl("doublon"), exit(1);
+			i++;
+		}
+		e++;
+	}
+}
+
+int		check_arg(int ac, char **av)
+{
+	int i;
+	int e;
+
+	i = 1;
+	while (ac > i)
+	{
+		e = 0;
+		while (av[i][e] != '\0')
+		{
+				if (ft_isdigit(av[i][e]) || (e == 0 && av[i][e] == '-' && \
+				ft_isdigit(av[i][e + 1])))
+				e++;
+			else
+			{
+				ft_putstr("BAD ARG");
+				return (0);
+			}
+		}
+		i++;
+	}
+	return 1;
+}
---
+++ src/resolve.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   resolve.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sksourou <sksourou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/04/10 18:40:45 by sksourou          #+#    #+#             */
+/*   Updated: 2015/04/10 18:40:46 by sksourou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+/*int	found_swap(int pos, t_ps *pile)
+{
+	if (pile->a[pos] == pile->ac - 1)
+		return (3);
+	else if ((pile->ac / 2) > pile->a[pos])
+		return (1);
+	else if ((pile->ac / 2) < pile->a[pos])
+		return (2);
+	return (0);
+}*/
+
+int found_max(t_ps *pile)
+{
+	int pos;
+	int nbr;
+	int ret;
+
+	pos = 1;
+	nbr = pile->a[0];
+	while (pos < pile->ac)
+	{
+		if (nbr < pile->a[pos])
+		{
+			nbr = pile->a[pos++];
+			ret = pos;
+		}
+		else if (pos < pile->ac)
+			pos++;
+	}
+	return (ret);
+}
+
+void	resolve(t_ps *pile)
+{
+	int pos;
+	int mode;
+
+	pos = found_max(pile);
+	//j = found_swap(pos, pile);
+	//swap_ra(pile, pos);
+	swap_a(pile);
+}
